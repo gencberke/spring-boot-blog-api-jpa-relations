6a8411aa39766ddd49e1a00d039e4ea986ebc615
gencberke <berkegenc.dev@gmail.com>
2025-10-30 10:23:27 +0300

    feat(controller): add UserController with CRUD and search endpoints

---
+++ src/main/java/com/berkedev/springbootblogapi/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.berkedev.springbootblogapi.controller;
+
+import com.berkedev.springbootblogapi.data.dto.request.UserCreateRequest;
+import com.berkedev.springbootblogapi.data.dto.request.UserUpdateRequest;
+import com.berkedev.springbootblogapi.data.dto.response.UserResponse;
+import com.berkedev.springbootblogapi.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getById(id));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserResponse> getByUsername(@PathVariable String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getByUsername(username));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserResponse> getByEmail(@PathVariable String email) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getByEmail(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> create(@Valid UserCreateRequest createRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(createRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest updateRequest) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(id, updateRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
